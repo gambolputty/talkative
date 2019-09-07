a798d3529133dbe21a5e029d572772c777a10ccb
Gregor <gregorweichbrodt@gmail.com>
2019-09-07 07:21:51 +0200

    wip

--- talkative/modifiers.py
+++ talkative/modifiers.py
@@ -1,3 +1,5 @@
+import re
+
 # Some modifiers by Allison Parrish: https://github.com/aparrish/pytracery/blob/master/tracery/modifiers.py
 
 def capitalize(text, *params):
@@ -8,14 +10,15 @@ def replace(text, *params):
     return text.replace(params[0], params[1])
 
 
-def choice(text, *params):
-    choices = [x for x in text.split('|')]
-    index = int(params[0]) - 1
-    return choices[index]
+def order(text, *params):
+    groups = re.findall(r'(?:\(([^)]+)\))', text)
+    indexes = [int(i) for i in params]
+    ordered = [groups[i] for i in indexes]
+    return ''.join(ordered)
 
 
 base = {
     'capitalize': capitalize,
     'replace': replace,
-    'choice': choice,
+    'order': order,
 }
